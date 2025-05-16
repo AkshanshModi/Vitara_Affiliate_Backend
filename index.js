74db424c0f8f68bcd63c5b7f1245f767f8e74555
Akshansh Modi <akshansh@prismetric.com>
2025-05-16 12:51:01 +0530

    save changes

--- index.js
+++ index.js
@@ -27,18 +27,7 @@ const corsOpts = {
     'Content-Type',
   ],
 };
-app.use(cors(corsOpts));
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true, // if you're using cookies or auth headers
-// }));
-
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect MongoDB
