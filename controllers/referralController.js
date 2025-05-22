2a03b79609144a33814f73c1fddc177767e8cad8
Akshansh Modi <akshansh@prismetric.com>
2025-05-22 12:45:24 +0530

    Referral links

--- controllers/referralController.js
+++ controllers/referralController.js
@@ -1,39 +1,44 @@
 const ReferralLink = require('../models/ReferralLink');
 
 exports.getReferralLinksAndStats = async (req, res) => {
-  try {
-    const userId = req.user._id;
-
-    const referralLinks = await ReferralLink.find({ user_id: userId });
-
-    let totalVisitors = 0;
-    let totalLeads = 0;
-    let totalConversions = 0;
-
-    const links = referralLinks.map(link => {
-      totalVisitors += link.visitors;
-      totalLeads += link.leads;
-      totalConversions += link.conversions;
-
-      return {
-        _id: link._id,
-        slug: link.slug,
-        visitors: link.visitors,
-        leads: link.leads,
-        conversions: link.conversions,
-        url: `https://vitara.dev?via=${link.slug}`
-      };
-    });
-
-    res.json({
-      total: {
-        visitors: totalVisitors,
-        leads: totalLeads,
-        conversions: totalConversions
-      },
-      links
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'Server error', details: err.message });
-  }
-};
+    try {
+      const userId = req.user._id;
+  
+      const referralLinks = await ReferralLink.find({ user_id: userId });
+  
+      if (!referralLinks || referralLinks.length === 0) {
+        return res.status(400).json({ message: 'No Referral links are available' });
+      }
+  
+      let totalVisitors = 0;
+      let totalLeads = 0;
+      let totalConversions = 0;
+  
+      const links = referralLinks.map(link => {
+        totalVisitors += link.visitors || 0;
+        totalLeads += link.leads || 0;
+        totalConversions += link.conversions || 0;
+  
+        return {
+          _id: link._id,
+          slug: link.slug,
+          visitors: link.visitors || 0,
+          leads: link.leads || 0,
+          conversions: link.conversions || 0,
+          url: `https://vitara.dev?via=${link.slug}`
+        };
+      });
+  
+      return res.json({
+        total: {
+          visitors: totalVisitors,
+          leads: totalLeads,
+          conversions: totalConversions
+        },
+        links
+      });
+    } catch (err) {
+      res.status(500).json({ error: 'Server error', details: err.message });
+    }
+  };
+  
